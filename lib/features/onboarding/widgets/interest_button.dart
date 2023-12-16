50a0d30d7f74b8d5fb04abeb3d21d2043477704e
Hazka3 <uryuryu.c@gmail.com>
2023-12-16 23:51:55 +0900

    Add action to interestsbutton
    When tapped, the background and text color of the widget changes.

--- lib/features/onboarding/widgets/interest_button.dart
+++ lib/features/onboarding/widgets/interest_button.dart
@@ -14,31 +14,46 @@ class InterestsButton extends StatefulWidget {
 }
 
 class _InterestsButtonState extends State<InterestsButton> {
+  bool _isSelected = false;
+
+  void _onTap() {
+    setState(() {
+      _isSelected = !_isSelected;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(
-        vertical: Sizes.size16,
-        horizontal: Sizes.size24,
-      ),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        border: Border.all(
-          color: Colors.black.withOpacity(0.1),
+    return GestureDetector(
+      onTap: _onTap,
+      child: AnimatedContainer(
+        duration: const Duration(
+          microseconds: 300,
         ),
-        borderRadius: BorderRadius.circular(Sizes.size32),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.05),
-            blurRadius: 5,
-            spreadRadius: 5,
+        padding: const EdgeInsets.symmetric(
+          vertical: Sizes.size16,
+          horizontal: Sizes.size24,
+        ),
+        decoration: BoxDecoration(
+          color: _isSelected ? Theme.of(context).primaryColor : Colors.white,
+          border: Border.all(
+            color: Colors.black.withOpacity(0.1),
           ),
-        ],
-      ),
-      child: Text(
-        widget.interest,
-        style: const TextStyle(
-          fontWeight: FontWeight.bold,
+          borderRadius: BorderRadius.circular(Sizes.size32),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.05),
+              blurRadius: 5,
+              spreadRadius: 5,
+            ),
+          ],
+        ),
+        child: Text(
+          widget.interest,
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: _isSelected ? Colors.white : Colors.black87,
+          ),
         ),
       ),
     );

3a6ad57ed4db3017690e40419673b49ed246fa51
Hazka3 <uryuryu.c@gmail.com>
2023-12-19 06:51:17 +0900

    Add /widgets/post_video.button.dart file

---
+++ lib/features/main_navigation/widgets/post_video_button.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PostVideoButton extends StatelessWidget {
+  const PostVideoButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}

2b5347af9e14286110af7166e1001bc6c01952a9
Hazka3 <uryuryu.c@gmail.com>
2023-12-20 06:00:27 +0900

    Add infinite scrolling logic
    Some test code is included, but will be deleted

--- lib/features/videos/video_timeline_screen.dart
+++ lib/features/videos/video_timeline_screen.dart
@@ -8,8 +8,63 @@ class VideoTimelineScreen extends StatefulWidget {
 }
 
 class _VideoTimelineScreenState extends State<VideoTimelineScreen> {
+  final PageController _pageController = PageController();
+
+  int _itemCount = 4;
+
+  List<Color> colors = [
+    Colors.blue,
+    Colors.red,
+    Colors.yellow,
+    Colors.teal,
+  ];
+
+  void _onPageChanged(int page) {
+    _pageController.animateToPage(
+      page,
+      duration: const Duration(milliseconds: 150),
+      curve: Curves.linear,
+    );
+    if (page == _itemCount - 1) {
+      _itemCount += 4;
+      colors.addAll(
+        [
+          Colors.blue,
+          Colors.red,
+          Colors.yellow,
+          Colors.teal,
+        ],
+      );
+    }
+    setState(() {});
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Stack();
+    return PageView.builder(
+      controller: _pageController,
+      scrollDirection: Axis.vertical,
+      itemCount: _itemCount,
+      onPageChanged: _onPageChanged,
+      itemBuilder: (context, index) {
+        return Container(
+          color: colors[index],
+          child: Center(
+            child: Text(
+              "Screen $index",
+              style: const TextStyle(
+                fontSize: 68,
+              ),
+            ),
+          ),
+        );
+      },
+    );
   }
 }

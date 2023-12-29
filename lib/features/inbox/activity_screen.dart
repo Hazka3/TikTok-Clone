6b8a73aa587c7fc4814dcc8008fe1714e9f1bc52
Hazka3 <uryuryu.c@gmail.com>
2023-12-30 06:47:14 +0900

    Add tap to turn animation for chevronDown icon in AppBar

--- lib/features/inbox/activity_screen.dart
+++ lib/features/inbox/activity_screen.dart
@@ -11,11 +11,29 @@ class ActivityScreen extends StatefulWidget {
   State<ActivityScreen> createState() => _ActivityScreenState();
 }
 
-class _ActivityScreenState extends State<ActivityScreen> {
+class _ActivityScreenState extends State<ActivityScreen>
+    with SingleTickerProviderStateMixin {
   final List<String> _notifications = List.generate(
     20,
     (index) => "${index}h",
   );
+
+  late final AnimationController _animationController = AnimationController(
+    vsync: this,
+    duration: const Duration(milliseconds: 200),
+  );
+
+  late final Animation<double> _animation = Tween(begin: 0.0, end: 0.5).animate(
+    _animationController,
+  );
+
+  void _onTitleTap() {
+    if (_animationController.isCompleted) {
+      _animationController.reverse();
+    } else {
+      _animationController.forward();
+    }
+  }
 
   void _onDismissed(String notification) {
     _notifications.remove(notification);
@@ -26,16 +44,22 @@ class _ActivityScreenState extends State<ActivityScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('All activity'),
-            Gaps.h2,
-            FaIcon(
-              FontAwesomeIcons.chevronDown,
-              size: Sizes.size14,
-            ),
-          ],
+        title: GestureDetector(
+          onTap: _onTitleTap,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text('All activity'),
+              Gaps.h2,
+              RotationTransition(
+                turns: _animation,
+                child: const FaIcon(
+                  FontAwesomeIcons.chevronDown,
+                  size: Sizes.size14,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
       body: ListView(

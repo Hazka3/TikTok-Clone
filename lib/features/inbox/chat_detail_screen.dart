3bab50c0e6669ffd46d925829f458543555a8f11
Hazka3 <uryuryu.c@gmail.com>
2024-01-02 05:38:38 +0900

    Add TextField in bottomAppBar (with text controll logic)

--- lib/features/inbox/chat_detail_screen.dart
+++ lib/features/inbox/chat_detail_screen.dart
@@ -11,6 +11,39 @@ class ChatDetailScreen extends StatefulWidget {
 }
 
 class _ChatDetailScreenState extends State<ChatDetailScreen> {
+  final TextEditingController _textEditingController = TextEditingController();
+  String _inputText = "";
+  bool _isWriting = false;
+
+  void _onStartWriting() {
+    setState(() {
+      _isWriting = true;
+    });
+  }
+
+  void _stopWriting() {
+    FocusScope.of(context).unfocus();
+    setState(() {
+      _isWriting = false;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _textEditingController.addListener(() {
+      setState(() {
+        _inputText = _textEditingController.text;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -76,53 +109,142 @@ class _ChatDetailScreenState extends State<ChatDetailScreen> {
           ),
         ),
       ),
-      body: Stack(
-        children: [
-          ListView.separated(
-            padding: const EdgeInsets.symmetric(
-              vertical: Sizes.size20,
-              horizontal: Sizes.size14,
+      body: GestureDetector(
+        onTap: _stopWriting,
+        child: Stack(
+          children: [
+            ListView.separated(
+              padding: const EdgeInsets.only(
+                right: Sizes.size20,
+                left: Sizes.size20,
+                top: Sizes.size14,
+                bottom: Sizes.size96 + Sizes.size20,
+              ),
+              itemBuilder: (context, index) {
+                final isMine = index % 2 == 0;
+                return Row(
+                  mainAxisAlignment:
+                      isMine ? MainAxisAlignment.end : MainAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.all(
+                        Sizes.size14,
+                      ),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                          topLeft: const Radius.circular(Sizes.size20),
+                          topRight: const Radius.circular(Sizes.size20),
+                          bottomLeft: Radius.circular(
+                            isMine ? Sizes.size20 : Sizes.size5,
+                          ),
+                          bottomRight: Radius.circular(
+                            isMine ? Sizes.size5 : Sizes.size20,
+                          ),
+                        ),
+                        color: isMine
+                            ? Colors.blue
+                            : Theme.of(context).primaryColor,
+                      ),
+                      child: const Text(
+                        "This is a message!",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: Sizes.size16,
+                        ),
+                      ),
+                    ),
+                  ],
+                );
+              },
+              separatorBuilder: (context, index) => Gaps.v10,
+              itemCount: 10,
             ),
-            itemBuilder: (context, index) {
-              final isMine = index % 2 == 0;
-              return Row(
-                mainAxisAlignment:
-                    isMine ? MainAxisAlignment.end : MainAxisAlignment.start,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Container(
-                    padding: const EdgeInsets.all(
-                      Sizes.size14,
-                    ),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.only(
-                        topLeft: const Radius.circular(Sizes.size20),
-                        topRight: const Radius.circular(Sizes.size20),
-                        bottomLeft: Radius.circular(
-                          isMine ? Sizes.size20 : Sizes.size5,
-                        ),
-                        bottomRight: Radius.circular(
-                          isMine ? Sizes.size5 : Sizes.size20,
-                        ),
-                      ),
-                      color:
-                          isMine ? Colors.blue : Theme.of(context).primaryColor,
-                    ),
-                    child: const Text(
-                      "This is a message!",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: Sizes.size16,
-                      ),
-                    ),
+            Positioned(
+              bottom: 0,
+              width: MediaQuery.of(context).size.width,
+              child: BottomAppBar(
+                color: Colors.grey.shade100,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: Sizes.size12,
+                    horizontal: Sizes.size18,
                   ),
-                ],
-              );
-            },
-            separatorBuilder: (context, index) => Gaps.v10,
-            itemCount: 10,
-          ),
-        ],
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: SizedBox(
+                          height: Sizes.size48,
+                          child: TextField(
+                            onTap: _onStartWriting,
+                            controller: _textEditingController,
+                            decoration: const InputDecoration(
+                              hintText: "Send a message...",
+                              contentPadding: EdgeInsets.only(
+                                left: Sizes.size16,
+                                top: Sizes.size10,
+                                bottom: Sizes.size10,
+                              ),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(Sizes.size20),
+                                  topRight: Radius.circular(Sizes.size20),
+                                  bottomLeft: Radius.circular(Sizes.size20),
+                                ),
+                                borderSide: BorderSide.none,
+                              ),
+                              filled: true,
+                              fillColor: Colors.white,
+                              suffixIcon: Padding(
+                                padding: EdgeInsets.only(
+                                  right: Sizes.size14,
+                                ),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    FaIcon(
+                                      FontAwesomeIcons.faceSmile,
+                                      color: Colors.black,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            autocorrect: false,
+                            keyboardType: TextInputType.multiline,
+                            expands: true,
+                            minLines: null,
+                            maxLines: null,
+                            textInputAction: TextInputAction.newline,
+                            cursorColor: Theme.of(context).primaryColor,
+                          ),
+                        ),
+                      ),
+                      Gaps.h16,
+                      _isWriting || _inputText.isNotEmpty
+                          ? Container(
+                              padding: const EdgeInsets.all(Sizes.size10),
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: _inputText.isNotEmpty
+                                    ? Theme.of(context).primaryColor
+                                    : Colors.grey.shade400,
+                              ),
+                              child: const FaIcon(
+                                FontAwesomeIcons.paperPlane,
+                                color: Colors.white,
+                                size: Sizes.size20,
+                              ),
+                            )
+                          : Container(),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }

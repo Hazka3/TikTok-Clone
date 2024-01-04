33a00df79544c5f497f3f809d960368903fa1558
Hazka3 <uryuryu.c@gmail.com>
2024-01-05 04:25:29 +0900

    Add Avatar Icon

--- lib/features/user/user_profile_screen.dart
+++ lib/features/user/user_profile_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 
 class UserProfileScreen extends StatefulWidget {
@@ -15,7 +16,7 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
     return CustomScrollView(
       slivers: [
         SliverAppBar(
-          title: const Text("は"),
+          title: const Text("はづか"),
           actions: [
             IconButton(
               onPressed: () {},
@@ -25,6 +26,20 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
               ),
             ),
           ],
+        ),
+        const SliverToBoxAdapter(
+          child: Column(
+            children: [
+              CircleAvatar(
+                radius: 50,
+                foregroundImage: NetworkImage(
+                  "https://avatars.githubusercontent.com/u/94900388?v=4",
+                ),
+                child: Text('はづか'),
+              ),
+              Gaps.v20,
+            ],
+          ),
         ),
       ],
     );

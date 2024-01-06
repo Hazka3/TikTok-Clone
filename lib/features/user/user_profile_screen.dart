c6b9e872f0a622af8dbfa5e127eb3d39f9471f0b
Hazka3 <uryuryu.c@gmail.com>
2024-01-07 00:42:38 +0900

    Add TabBar
    - random movie list
    - like ranking

--- lib/features/user/user_profile_screen.dart
+++ lib/features/user/user_profile_screen.dart
@@ -15,153 +15,188 @@ class UserProfileScreen extends StatefulWidget {
 class _UserProfileScreenState extends State<UserProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(
-      slivers: [
-        SliverAppBar(
-          title: const Text("はづか"),
-          actions: [
-            IconButton(
-              onPressed: () {},
-              icon: const FaIcon(
-                FontAwesomeIcons.gear,
-                size: Sizes.size20,
-              ),
-            ),
-          ],
-        ),
-        SliverToBoxAdapter(
-          child: Column(
-            children: [
-              const CircleAvatar(
-                radius: 50,
-                foregroundImage: NetworkImage(
-                  "https://avatars.githubusercontent.com/u/94900388?v=4",
-                ),
-                child: Text('はづか'),
-              ),
-              Gaps.v14,
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text(
-                    '@Hazka',
-                    style: TextStyle(
-                      fontWeight: FontWeight.w600,
-                      fontSize: Sizes.size18,
-                    ),
-                  ),
-                  Gaps.h5,
-                  FaIcon(
-                    FontAwesomeIcons.solidCircleCheck,
-                    size: Sizes.size16,
-                    color: Colors.blue.shade500,
-                  ),
-                ],
-              ),
-              Gaps.v14,
-              SizedBox(
-                height: Sizes.size48,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const FollowCount(
-                      count: 37,
-                      title: "Following",
-                    ),
-                    VerticalDivider(
-                      color: Colors.grey.shade300,
-                      width: Sizes.size32,
-                      thickness: Sizes.size1,
-                      indent: Sizes.size14,
-                      endIndent: Sizes.size14,
-                    ),
-                    const FollowCount(
-                      count: 10525326,
-                      title: "Followers",
-                    ),
-                    VerticalDivider(
-                      color: Colors.grey.shade300,
-                      width: Sizes.size32,
-                      thickness: Sizes.size1,
-                      indent: Sizes.size14,
-                      endIndent: Sizes.size14,
-                    ),
-                    const FollowCount(
-                      count: 149253236,
-                      title: "Likes",
-                    ),
-                  ],
+    return DefaultTabController(
+      length: 2,
+      child: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            title: const Text("はづか"),
+            actions: [
+              IconButton(
+                onPressed: () {},
+                icon: const FaIcon(
+                  FontAwesomeIcons.gear,
+                  size: Sizes.size20,
                 ),
               ),
-              Gaps.v14,
-              const Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: Sizes.size72 + Sizes.size4,
-                ),
-                child: Row(
-                  children: [
-                    SocialButton(
-                      buttonType: ButtonType.followButton,
-                      child: Text(
-                        'Follow',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.w600,
-                        ),
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                    Gaps.h6,
-                    SocialButton(
-                      buttonType: ButtonType.iconButton,
-                      child: FaIcon(
-                        FontAwesomeIcons.youtube,
-                        color: Colors.black,
-                      ),
-                    ),
-                    Gaps.h6,
-                    SocialButton(
-                      buttonType: ButtonType.iconButton,
-                      child: FaIcon(
-                        FontAwesomeIcons.ellipsis,
-                        color: Colors.black,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Gaps.v14,
-              const Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: Sizes.size32,
-                ),
-                child: Text(
-                  'All highlights and where to watch live matches on FIFA+ I wonder how it would loooooooooooook',
-                  textAlign: TextAlign.center,
-                ),
-              ),
-              Gaps.v14,
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  FaIcon(
-                    FontAwesomeIcons.link,
-                    size: Sizes.size12,
-                  ),
-                  Gaps.h4,
-                  Text(
-                    "https://github.com/Hazka3",
-                    style: TextStyle(
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                ],
-              ),
-              Gaps.v5,
             ],
           ),
-        ),
-      ],
+          SliverToBoxAdapter(
+            child: Column(
+              children: [
+                const CircleAvatar(
+                  radius: 50,
+                  foregroundImage: NetworkImage(
+                    "https://avatars.githubusercontent.com/u/94900388?v=4",
+                  ),
+                  child: Text('はづか'),
+                ),
+                Gaps.v14,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      '@Hazka',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: Sizes.size18,
+                      ),
+                    ),
+                    Gaps.h5,
+                    FaIcon(
+                      FontAwesomeIcons.solidCircleCheck,
+                      size: Sizes.size16,
+                      color: Colors.blue.shade500,
+                    ),
+                  ],
+                ),
+                Gaps.v14,
+                SizedBox(
+                  height: Sizes.size48,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const FollowCount(
+                        count: 37,
+                        title: "Following",
+                      ),
+                      VerticalDivider(
+                        color: Colors.grey.shade300,
+                        width: Sizes.size32,
+                        thickness: Sizes.size1,
+                        indent: Sizes.size14,
+                        endIndent: Sizes.size14,
+                      ),
+                      const FollowCount(
+                        count: 10525326,
+                        title: "Followers",
+                      ),
+                      VerticalDivider(
+                        color: Colors.grey.shade300,
+                        width: Sizes.size32,
+                        thickness: Sizes.size1,
+                        indent: Sizes.size14,
+                        endIndent: Sizes.size14,
+                      ),
+                      const FollowCount(
+                        count: 149253236,
+                        title: "Likes",
+                      ),
+                    ],
+                  ),
+                ),
+                Gaps.v14,
+                const Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: Sizes.size72 + Sizes.size4,
+                  ),
+                  child: Row(
+                    children: [
+                      SocialButton(
+                        buttonType: ButtonType.followButton,
+                        child: Text(
+                          'Follow',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w600,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      Gaps.h6,
+                      SocialButton(
+                        buttonType: ButtonType.iconButton,
+                        child: FaIcon(
+                          FontAwesomeIcons.youtube,
+                          color: Colors.black,
+                        ),
+                      ),
+                      Gaps.h6,
+                      SocialButton(
+                        buttonType: ButtonType.iconButton,
+                        child: FaIcon(
+                          FontAwesomeIcons.ellipsis,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Gaps.v14,
+                const Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: Sizes.size32,
+                  ),
+                  child: Text(
+                    'All highlights and where to watch live matches on FIFA+ I wonder how it would loooooooooooook',
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Gaps.v14,
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    FaIcon(
+                      FontAwesomeIcons.link,
+                      size: Sizes.size12,
+                    ),
+                    Gaps.h4,
+                    Text(
+                      "https://github.com/Hazka3",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ],
+                ),
+                Gaps.v20,
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.symmetric(
+                      horizontal: BorderSide(
+                        color: Colors.grey.shade200,
+                        width: 0.5,
+                      ),
+                    ),
+                  ),
+                  child: const TabBar(
+                    indicatorColor: Colors.black,
+                    indicatorSize: TabBarIndicatorSize.label,
+                    labelColor: Colors.black,
+                    labelPadding: EdgeInsets.symmetric(
+                      vertical: Sizes.size10,
+                    ),
+                    tabs: [
+                      Padding(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: Sizes.size12,
+                        ),
+                        child: Icon(Icons.grid_4x4_rounded),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: Sizes.size12,
+                        ),
+                        child: FaIcon(FontAwesomeIcons.heart),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }

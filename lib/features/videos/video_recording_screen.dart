a448dd1c993733d7ab311e912b38ae2bd2c24b0e
Hazka3 <uryuryu.c@gmail.com>
2024-01-16 07:50:28 +0900

    Add camera and microphone permissions popup to display

--- lib/features/videos/video_recording_screen.dart
+++ lib/features/videos/video_recording_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class VideoRecordingScreen extends StatefulWidget {
   const VideoRecordingScreen({super.key});
@@ -8,6 +9,30 @@ class VideoRecordingScreen extends StatefulWidget {
 }
 
 class _VideoRecordingScreenState extends State<VideoRecordingScreen> {
+  bool _hasPermission = false;
+
+  Future<void> initPermission() async {
+    final cameraPermission = await Permission.camera.request();
+    final micPermission = await Permission.microphone.request();
+
+    final cameraDenied =
+        cameraPermission.isDenied || cameraPermission.isPermanentlyDenied;
+    final micDenied =
+        micPermission.isDenied || micPermission.isPermanentlyDenied;
+
+    if (!cameraDenied && micDenied) {
+      setState(() {
+        _hasPermission = true;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initPermission();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(

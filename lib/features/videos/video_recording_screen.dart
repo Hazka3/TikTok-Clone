bbd600b6d9ced6d8a41854ee5de8f24cdc619eca
Hazka3 <uryuryu.c@gmail.com>
2024-01-16 23:05:47 +0900

    Add camera/mic permission

--- lib/features/videos/video_recording_screen.dart
+++ lib/features/videos/video_recording_screen.dart
@@ -1,5 +1,7 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
 
 class VideoRecordingScreen extends StatefulWidget {
   const VideoRecordingScreen({super.key});
@@ -11,6 +13,25 @@ class VideoRecordingScreen extends StatefulWidget {
 class _VideoRecordingScreenState extends State<VideoRecordingScreen> {
   bool _hasPermission = false;
 
+  late final CameraController _cameraController;
+
+  Future<void> initCamera() async {
+    // 使用可能なカメラ（フロントカメラ、バックカメラなど）のリストを受け取る
+    final cameras = await availableCameras();
+
+//デバイスにカメラがない場合
+    if (cameras.isEmpty) {
+      return;
+    }
+
+    _cameraController = CameraController(
+      cameras[0],
+      ResolutionPreset.ultraHigh,
+    );
+
+    await _cameraController.initialize();
+  }
+
   Future<void> initPermission() async {
     final cameraPermission = await Permission.camera.request();
     final micPermission = await Permission.microphone.request();
@@ -20,10 +41,10 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen> {
     final micDenied =
         micPermission.isDenied || micPermission.isPermanentlyDenied;
 
-    if (!cameraDenied && micDenied) {
-      setState(() {
-        _hasPermission = true;
-      });
+    if (!cameraDenied && !micDenied) {
+      await initCamera();
+      _hasPermission = true;
+      setState(() {});
     }
   }
 
@@ -37,7 +58,29 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Container(),
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child: !_hasPermission || !_cameraController.value.isInitialized
+            ? const Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    "Requesting permissions...",
+                  ),
+                  Gaps.v20,
+                  CircularProgressIndicator.adaptive(),
+                ],
+              )
+            : Stack(
+                alignment: Alignment.center,
+                children: [
+                  CameraPreview(
+                    _cameraController,
+                  )
+                ],
+              ),
+      ),
     );
   }
 }

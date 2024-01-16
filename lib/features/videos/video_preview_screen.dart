d0b16b294400f77bbab1785bfeffb02aca31a01c
Hazka3 <uryuryu.c@gmail.com>
2024-01-17 05:16:51 +0900

    Add video_preview_screen.dart file

---
+++ lib/features/videos/video_preview_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class VideoPreviewScreen extends StatefulWidget {
+  const VideoPreviewScreen({super.key});
+
+  @override
+  State<VideoPreviewScreen> createState() => _VideoPreviewScreenState();
+}
+
+class _VideoPreviewScreenState extends State<VideoPreviewScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

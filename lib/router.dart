fa360b0cc7d4f95b6e3c977a41d6d37950d8ba99
Hazka3 <uryuryu.c@gmail.com>
2024-01-18 23:39:35 +0900

    Add router from SignUpScreen to LoginScreen

--- lib/router.dart
+++ lib/router.dart
@@ -1,4 +1,5 @@
 import 'package:go_router/go_router.dart';
+import 'package:tiktok_clone/features/authentication/login_screen.dart';
 import 'package:tiktok_clone/features/authentication/sign_up_screen.dart';
 
 final router = GoRouter(
@@ -7,6 +8,11 @@ final router = GoRouter(
       name: SignUpScreen.routeName,
       path: SignUpScreen.routeURL,
       builder: (context, state) => const SignUpScreen(),
+    ),
+    GoRoute(
+      name: LoginScreen.routeName,
+      path: LoginScreen.routeURL,
+      builder: (context, state) => const LoginScreen(),
     ),
   ],
 );

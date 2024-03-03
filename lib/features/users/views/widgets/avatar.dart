5d085ce5c0cbf9782fcff84a16ab4fb53b09dde0
Hazka3 <uryuryu.c@gmail.com>
2024-03-04 00:27:00 +0900

    Add /users/views/widgets/avatar.dart file
    Extracted from userProfileScreen>CircleAvatar widget

---
+++ lib/features/users/views/widgets/avatar.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class Avatar extends ConsumerWidget {
+  const Avatar({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Container();
+  }
+}

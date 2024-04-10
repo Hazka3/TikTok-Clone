ecb6e7e2642de3e6d6269b4112b11ae048d617e2
Hazka3 <uryuryu.c@gmail.com>
2024-04-11 00:51:13 +0900

    feat: Add a function to extract thumbnails from videos
    Using ffmpeg from child-process-promise (npm)

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -8,5 +8,27 @@ admin.initializeApp();
 export const onVideoCreated = functions.firestore
   .document("/videos/{videoId}")
   .onCreate(async (snapshot, context) => {
-    await snapshot.ref.update({ hello: "from functions" });
+    const spawn = require("child-process-promise").spawn;
+    const video = snapshot.data(); //snapshot => ここで生成したdocument
+    await spawn("ffmpeg", [
+      //npm > child-process-promissを使ってffmpegを実行
+      //ffmpeg のコマンド（-iや-ssなど）は適宜検索してみる
+
+      "-i", //input 命令
+      video.fileUrl, //inputするlocation (url)、video_modelの"fileUrl" parameter を参照
+      "-ss", //切り出しの開始位置に移動
+      "00:00:01.000", //開始位置 = 再生1秒〜
+      "-vframes", //静止画を切り出し
+      "1", //切り出したい静止画の枚数
+      "-vf", //ビデオフィルターを設定
+      "scale=150:-1", //フィルター＞スケールを設定（容量削減＋リサイズのため）　縦:横、-1ならもう片方の値合わせて自動で調整される
+      `/tmp/${snapshot.id}.jpg`,
+      //一時保存ストレージにファイル名を指定して保存
+      //cloud functionで生成されたファイルはtmpフォルダーに一時保存され、関数の実行が完了すると消去される。なので、リアルタイムで参照するstateは入れないこと
+    ]);
+    const storage = admin.storage();
+    await storage.bucket().upload(`/tmp/${snapshot.id}.jpg`, {
+      //storageにアップロードするファイルを指定　（ここでは一時保存storageから持ってきている）
+      destination: `thumbnails/${snapshot.id}.jpg`, //storage/thumbnails/{snapshot.id}.jpg としてファイルを保存
+    });
   });

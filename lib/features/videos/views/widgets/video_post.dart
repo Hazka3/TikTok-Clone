7b3b8e5cf400197a14058763d56977732d05545c
Hazka3 <uryuryu.c@gmail.com>
2024-04-29 21:53:13 +0900

    docs: Move vm file to correct path
    - video_post_view_models.dart ('/models' => '/view_models')

--- lib/features/videos/views/widgets/video_post.dart
+++ lib/features/videos/views/widgets/video_post.dart
@@ -6,7 +6,7 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 import 'package:tiktok_clone/features/videos/models/video_model.dart';
-import 'package:tiktok_clone/features/videos/models/video_post_view_models.dart';
+import 'package:tiktok_clone/features/videos/view_models/video_post_view_models.dart';
 import 'package:tiktok_clone/features/videos/view_models/playback_config_vm.dart';
 import 'package:tiktok_clone/features/videos/views/widgets/video_button.dart';
 import 'package:tiktok_clone/features/videos/views/widgets/video_comments.dart';

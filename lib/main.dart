ca63626a2d126a77d44250febeed65437ddd35a6
Hazka3 <uryuryu.c@gmail.com>
2024-06-03 03:25:27 +0900

    feat: User's token can be stored in db.

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:tiktok_clone/common/widgets/settings/common_setting.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/notifications/notifications_provider.dart';
 import 'package:tiktok_clone/features/videos/repos/playback_config_repo.dart';
 import 'package:tiktok_clone/features/videos/view_models/playback_config_vm.dart';
 import 'package:tiktok_clone/firebase_options.dart';
@@ -42,6 +43,7 @@ class TikTokApp extends ConsumerWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    ref.watch(notificationsProvider);
     S.load(const Locale('en'));
     return ValueListenableBuilder(
       valueListenable: darkModeConfig,
